e1953d646e0055b9b6d9fcac377161688c13918d
Dustin <dustin.flegel@globalpayments.com>
2026-02-15 00:16:37 -0500

    Bump version to v0.6.0 Alpha6

--- public/nav.js
+++ public/nav.js
@@ -94,8 +94,8 @@
       </div>
       <nav class="sidebar-nav">${navLinks}</nav>
       <div class="sidebar-version">
-        <span class="version-number">v0.5.0</span>
-        <span class="version-tag">Alpha5</span>
+        <span class="version-number">v0.6.0</span>
+        <span class="version-tag">Alpha6</span>
       </div>
     `;
 
